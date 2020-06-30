9f39e28975d087f2dcd29441c927f7758ed86365
GitHub Action <action@github.com>
2020-06-30 19:19:20 +0000

    Template cleanup

--- src/main/kotlin/com/github/kindermax/intellijlets/MyBundle.kt
+++ src/main/kotlin/com/github/kindermax/intellijlets/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.kindermax.intellijlets
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/kindermax/intellijlets/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/kindermax/intellijlets/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.kindermax.intellijlets.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.kindermax.intellijlets.MyBundle
 
 class MyApplicationService {
 
