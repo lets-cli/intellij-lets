6fbd881c578a70915528e77cd4d767bfb7127d86
Kindritskiy Max <kindritskiy.m@gmail.com>
2020-07-07 13:45:28 +0300

    replace png filtype icon with svg, add pluginIcon.svg

--- src/main/kotlin/com/github/kindermax/intellijlets/Icons.kt
+++ src/main/kotlin/com/github/kindermax/intellijlets/Icons.kt
@@ -4,5 +4,5 @@ import com.intellij.openapi.util.IconLoader
 import javax.swing.Icon
 
 object Icons  {
-    val LetsYaml: Icon by lazy { IconLoader.getIcon("/icons/lets.png") }
+    val LetsYaml: Icon by lazy { IconLoader.getIcon("/icons/lets.svg") }
 }
