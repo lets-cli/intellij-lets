1b6d5df1bfdef85e6529f3b1567f744f8c2c7c1c
Max Kindritskiy <kindritskiy.m@gmail.com>
2021-04-10 17:09:38 +0300

    add icon for completion variant

--- src/main/kotlin/com/github/kindermax/intellijlets/LetsCompletionProvider.kt
+++ src/main/kotlin/com/github/kindermax/intellijlets/LetsCompletionProvider.kt
@@ -3,6 +3,7 @@ package com.github.kindermax.intellijlets
 import com.intellij.codeInsight.completion.CompletionParameters
 import com.intellij.codeInsight.completion.CompletionProvider
 import com.intellij.codeInsight.completion.CompletionResultSet
+import com.intellij.codeInsight.lookup.LookupElement
 import com.intellij.codeInsight.lookup.LookupElementBuilder
 import com.intellij.util.ProcessingContext
 
@@ -16,19 +17,25 @@ object LetsCompletionProvider : CompletionProvider<CompletionParameters>() {
     ) {
         when {
             configUtils.isRootLevel(parameters) -> {
-                result.addAllElements(TOP_LEVEL_KEYWORDS.map { keyword -> LookupElementBuilder.create(keyword) })
+                result.addAllElements(TOP_LEVEL_KEYWORDS.map { keyword -> createLookupElement(keyword) })
             }
             configUtils.isCommandLevel(parameters) -> {
-                result.addAllElements(COMMAND_LEVEL_KEYWORDS.map { keyword -> LookupElementBuilder.create(keyword) })
+                result.addAllElements(COMMAND_LEVEL_KEYWORDS.map { keyword -> createLookupElement(keyword) })
             }
             configUtils.isDependsLevel(parameters) -> {
                 result.addAllElements(
                     configUtils.getCommandsNames(parameters).map {
                         keyword ->
-                        LookupElementBuilder.create(keyword)
+                        createLookupElement(keyword)
                     }
                 )
             }
         }
+    }
+
+    private fun createLookupElement(text: String): LookupElement {
+        return LookupElementBuilder
+            .create(text)
+            .withIcon(Icons.LetsYaml)
     }
 }
